dbe42c96eab6997dcb6ce49e97a154567520014a
Ayush Neekhar <a.neekhar01@gmail.com>
2025-12-03 22:52:08 +0530

    More boiler plate for example app as well as library configs

--- react-native-nitro-torrent/cpp/HybridLibtorrent.hpp
+++ react-native-nitro-torrent/cpp/HybridLibtorrent.hpp
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <libtorrent/session.hpp>
+#include <thread>
+
 #include "HybridLibtorrentSpec.hpp"
+#include "libtorrent/sha1_hash.hpp"
 
 namespace margelo::nitro::nitrotorrent {
 class HybridLibtorrent : public HybridLibtorrentSpec {
    public:
     HybridLibtorrent() : HybridObject(TAG) {}
+    virtual ~HybridLibtorrent();
 
    public:
     void addMagnetLink(const std::string& magnetLink) override;
@@ -14,5 +19,14 @@ class HybridLibtorrent : public HybridLibtorrentSpec {
     void cancelTorrent(const std::string& torrentId) override;
     void deleteTorrent(const std::string& torrentId) override;
     Torrent getTorrent(const std::string& torrentId) override;
+
+   private:
+    lt::sha1_hash getInfoHash(const std::string& torrentId);
+
+    lt::session m_session;
+
+    std::thread m_alertThread;
+    std::atomic<bool> m_running{false};
+    void alertThread();
 };
 }  // namespace margelo::nitro::nitrotorrent
